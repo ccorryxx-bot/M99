6331f55180f0834e4a0f48d3cd285be207feb558
KYAW GYII - YFM <ccorryxx@gmail.com>
2026-06-10 04:42:56 +0630

    fix: ကြေညာချက်များ button redirect to CS announce tab

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -162,7 +162,7 @@
             </div>
             <span class="gi-lbl">ဖောက်သည်<br>ဝန်ဆောင်မှု</span>
           </button>
-          <button class="gi-item" @click="comingSoon">
+          <button class="gi-item" @click="showCs=true; csTab='announce'">
             <div class="gi-icon-plain">
               <svg width="22" height="22" fill="none" viewBox="0 0 24 24" stroke="rgba(255,255,255,0.80)" stroke-width="1.7"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"/></svg>
             </div>
